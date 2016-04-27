bf96d3c0383cefee44fd9500d5fa64120355a3ef
Tanner Burke <tmb377@nyu.edu>
2016-04-27 13:07:35 -0400

    new changes

--- load-labels.py
+++ load-labels.py
@@ -1,8 +1,8 @@
-import cPickle as pickle
+import pickle
 # import project
 from sklearn.cluster import KMeans
 
 labels = pickle.load(open('labels.p','rb'))
 
 kmeans = KMeans()
-print kmeans.get_params(labels.all())
+print(kmeans.get_params(labels.all()))

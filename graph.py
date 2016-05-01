46633708b8a3bd1e2a4b8a124b1bd1c1659334ff
saraoswald <sara.linsley@gmail.com>
2016-05-01 15:05:55 -0400

    Fixed graphing Complaint Type

--- graph.py
+++ graph.py
@@ -3,15 +3,11 @@ import pylab
 
 
 data = km.get_data()
-kmeans = km.get_kmeans(data,2)
+kmeans = km.get_kmeans(data,10)
 clusters = kmeans.cluster_centers_
 
-<<<<<<< HEAD
-x,y = 5,104
-=======
 
-x,y = 27,28
->>>>>>> ce7f19d28bcebeb2c1341395d190b0b680895d92
+x,y = 1,2
 
 pylab.scatter(data[:,x],data[:,y],color='green')
 pylab.scatter(clusters[:,x],clusters[:,y], marker='x', s = 500, linewidths=2)

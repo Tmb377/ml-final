1b208b25868c175133561317a05605f2c1e93598
Tanner Burke <tmb377@nyu.edu>
2016-05-01 19:27:18 -0400

    PCA reduction
    Added PCA reduction to data in graph.py

--- graph.py
+++ graph.py
@@ -1,13 +1,19 @@
 import kmeans as km
 import pylab
+from sklearn.decomposition import PCA
 
 
 data = km.get_data()
 
+#reduce dimensions
+pca = PCA(n_components=2)
+data = pca.fit_transform(data)
+for line in data:
+	print(line[0])
 labels, kmeans = km.get_kmeans(data,10)
 clusters = kmeans.cluster_centers_
 
-columns = ['Unique Key', 'Complaint Type',  'Latitude', 'Longitude', 'month',
+columns = ['Unique Key', 'Complaint Type', 'Incident Zip', 'Latitude', 'Longitude', 'month',
        'year', 'hour', 'AM', 'Agency_3-1-1', 'Agency_CHALL',
        'Agency_DCA', 'Agency_DEP', 'Agency_DFTA', 'Agency_DHS',
        'Agency_DOB', 'Agency_DOE', 'Agency_DOF', 'Agency_DOHMH',
@@ -17,12 +23,14 @@ columns = ['Unique Key', 'Complaint Type',  'Latitude', 'Longitude', 'month',
        'Status_Email Sent', 'Status_Open', 'Status_Pending',
        'Status_Started']
 
-x,y = 1,2
+#x,y = 4, 3
 
-
-pylab.scatter(data[:,x],data[:,y],color='green')
-pylab.scatter(clusters[:,x],clusters[:,y], marker='x', s = 500, linewidths=2)
-pylab.xlabel(columns[x])
-pylab.ylabel(columns[y])
-
+colors = ([([0.4,1,0.4],[1,0.4,0.4],[0.1,0.8,1], [.1, 1, 0], [.5, .6, .2], [.2, .6, .5], [.6, .2, .5], [0, .9, .0], [.9, 0, 0], [0, 0, .9])[i] for i in labels])
+pylab.scatter(data[:,0],data[:,1],color=colors)
+pylab.scatter(clusters[:,0],clusters[:,1], marker='x', s = 500, linewidths=2)
+pylab.xlabel(columns[0])
+pylab.ylabel(columns[1])
+#pylab.ylim([9750,12000])
+#pylab.xlim([-10, 160])
 pylab.show()
+

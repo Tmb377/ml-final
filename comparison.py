4be1a78384298c8f15bb1830f84a7588cf97b7af
Tanner Burke <tmb377@nyu.edu>
2016-05-01 14:13:34 -0400

    Added comparison.py
    Compares kmeans and affinity propagation performance

---
+++ comparison.py
@@ -0,0 +1,15 @@
+import kmeans as clustering
+
+
+
+data = clustering.get_data()
+
+km_labels = clustering.get_kmeans(data,20)
+
+aff_labels = clustering.get_affinity(data)
+
+print("Number of affinity clusters", len(set(aff_labels)))
+
+print("Score for Kmeans:", clustering.get_silhouette(data, km_labels))
+
+print("Score for Affinity:", clustering.get_silhouette(data, aff_labels))
